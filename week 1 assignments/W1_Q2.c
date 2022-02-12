017b7eac8016ba64bba148e43f2f8b6e781bffb2
Jyoti Prakash Panda <96424607+jyotiprakashh@users.noreply.github.com>
2022-02-12 18:36:47 +0530

    Update W1_Q2.c

--- week 1 assignments/W1_Q2.c
+++ week 1 assignments/W1_Q2.c
@@ -5,6 +5,8 @@ int main(){
     scanf("%d %d",&a,&b);
     if(a>b)
     printf("%d is the greatest",a);
+    else if (a==b)
+    printf("Both numbers are equal");
     else
     printf("%d is the greatest",b);
     return 0;
